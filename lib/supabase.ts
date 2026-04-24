fd07dfbdf6bc8733a60221ed30c9647ead345233
Ahmad Khan <khanyousafzaideveloper@gmail.com>
2026-04-24 18:16:25 +0500

    Fix lib/supabase.ts to use fallback values

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://ukcpgpbhfpiyaekvgran.supabase.co"
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrY3BncGJoZnBpeWFla3ZncmFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NTQ0MDQsImV4cCI6MjA5MjUzMDQwNH0._zNQCsl9x1fITmHl5JGqO3MLaxVR46KFTfmzhARldzg"
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.warn(
@@ -10,6 +10,6 @@ if (!supabaseUrl || !supabaseAnonKey) {
 }
 
 export const supabase = createClient(
-  supabaseUrl || "",
-  supabaseAnonKey || "",
+  supabaseUrl,
+  supabaseAnonKey,
 )
